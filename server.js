ddd7de9f132350d5430140481133a02d82bf3c9b
Ubuntu <ubuntu@ip-172-31-36-195.eu-west-2.compute.internal>
2020-03-12 23:43:49 +0000

    a

--- server.js
+++ server.js
@@ -11,46 +11,6 @@ app.get('/', function(req, res) {
 
 app.use(express.static("public"));
 
-// create routes for the admin section
-//get an instance of the router
-var adminRouter = express.Router();
-// admin main page. the dashboard (http://localhost:PORT/admin)
-adminRouter.get('/', function(req, res) {
- res.send('I am the dashboard!'); });
-// users page (http://localhost:PORT/admin/users)
-adminRouter.get('/users', function(req, res) {
- res.send('I show all the users!'); });
-// posts page (http://localhost:PORT/admin/posts)
-adminRouter.get('/posts', function(req, res) {
- res.send('I show all the posts!'); });
-// apply the routes to our application
-app.use('/admin', adminRouter);
-
-// route middleware that will happen on every request
- adminRouter.use(function(req, res, next) {
-  // log each request to the console
-  console.log(req.method, req.url);
-  // continue doing what we were doing and go to the route
-  next(); });
-
-
-// route middleware to validate :name
-adminRouter.param('name', function(req, res, next, name) {
- // do validation on name here
- // log something so we know its working
- console.log('doing name validations on ' + name);
- // once validation is done save the new item in the req
- req.name = name;
- // go to the next thing
- next();
- });
-
-
-
-// route with parameters (http://localhost:PORT/admin/users/:name)
-adminRouter.get('/users/:name', function(req, res) {
- res.send('hello ' + req.params.name + '!'); });
-
 app.route('/login')
  // show the form (GET http://localhost:PORT/login)
  .get(function(req, res) {
@@ -58,7 +18,10 @@ app.route('/login')
  })
  // process the form (POST http://localhost:PORT/login)
  .post(function(req, res) {
- console.log('processing');
+ var output = "processing"
+ var input1 = req.query.input1;
+ var input2 = req.query.input2;
+ console.log("The params:" + input1 + " " + input2);
  res.send('processing the login form!');
  });
 
