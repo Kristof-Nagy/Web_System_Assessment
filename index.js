36594a1f0d6bbe197ff9cc7c638e66a6ca9e9be3
Ubuntu <ubuntu@ip-172-31-34-92.eu-west-1.compute.internal>
2020-04-01 23:57:44 +0000

    i

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ var http = require("http");
 fs = require("fs");
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
-  console.log("our app is running on port ${PORT}");
-});
-
 http.createServer(function(req, res)
 {
   res.writeHead(200, {"Content-Type": "text/plain",
