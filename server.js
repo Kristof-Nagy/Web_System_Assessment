bac76299894a971f6a61bf85d556cf68f78984f2
Ubuntu <ubuntu@ip-172-31-34-92.eu-west-1.compute.internal>
2020-04-03 20:19:32 +0000

    a

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var app     = express();
 const session = require("express-session");
-const mongo_store = require("connect-mongo")(session);
+const MongoStore = require("connect-mongo")(session);
 
 
 const PORT = process.env.PORT || 8080;
