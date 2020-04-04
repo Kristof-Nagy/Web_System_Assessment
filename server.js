eb8846a131385bbae14e6f9f241cbe69edc10ec4
Ubuntu <ubuntu@ip-172-31-34-92.eu-west-1.compute.internal>
2020-04-04 16:25:16 +0000

    A

--- server.js
+++ server.js
@@ -166,7 +166,7 @@ app.route("/user")
 app.route("/logout")
  .get(function(req, res){
 	res.json( {destroy:"session will be destroyed"});
-	sendFile(__dirname + '/game.html');
+	res.sendFile(__dirname + '/game.html');
  });
 
 // start the server
